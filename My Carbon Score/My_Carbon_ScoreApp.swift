0e1a9f51206f9e5f174e786cb054af9dc594f899
m9m <m9m@users.noreply.github.com>
2023-08-25 21:02:13 -0500

    Initial Commit

---
+++ My Carbon Score/My_Carbon_ScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_Carbon_ScoreApp.swift
+//  My Carbon Score
+//
+//  Created by Keegan Murphy on 8/25/23.
+//
+
+import SwiftUI
+
+@main
+struct My_Carbon_ScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
